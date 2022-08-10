6b04a24d4125bd52a72e3db6757c7badd708ed1e
KentaHorai <chickentyan1023@gmail.com>
2022-08-10 14:07:10 +0900

    classBase

--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,4 +1,10 @@
 <x-tests.app>
     <x-slot name="header">ヘッダー2</x-slot>
-コンポーネントテスト2
+    コンポーネントテスト2
+
+    <x-test-class-base classBaseMessage="クラスベースのメッセージです。"/>
+
+    <div class="mb-4"></div>
+
+    <x-test-class-base classBaseMessage="クラスベースのメッセージです。" defaultMessage="初期値から変更しています。"/>
 </x-tests.app>
